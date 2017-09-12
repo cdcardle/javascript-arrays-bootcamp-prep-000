b5ea649da2f9b1313a2af12ef917788346eb82a8
Chris Cardle <cdcardle@gmail.com>
2017-09-12 17:33:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = new array('snickers', 'hundred grand', 'kitkat', 'skittles')
+
+function addElementToBeginningOfArray(element, array) {
+  var n = 1
+  const array + `n` = [...array, "element"]
+  n = n + 1
+}
